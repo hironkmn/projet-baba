4235440ebbbf335bf6f27fad4979302eb3e5ae79
mzerrouk <100277006+mzerrouk@users.noreply.github.com>
2022-05-16 16:44:21 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: 'Quel est le résultat de 2 + 3 ?',
+        question: 'Quel est le résultat de 2 + 2 ?',
         choice1: '2',
         choice2: '4',
         choice3: '8',
