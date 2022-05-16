1aa7e9a982765ea0967609a86d88be3bf7ef28ca
mzerrouk <meidine59@gmail.com>
2022-05-16 16:39:28 +0200

    Add JS

--- js/game.js
+++ js/game.js
@@ -1,1 +1,93 @@
 const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'Quel est le résultat de 2 + 2 ?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '8',
+        choice4: '112',
+        answer: 2,
+    },
+    {
+        question: 'Qui est mort à la fin de Endgame ?',
+        choice1: 'Captain America',
+        choice2: 'Bruce Banner',
+        choice3: 'Iron Man',
+        choice4: 'Dark Vador',
+        answer: 3,
+    },
+    {
+        question: 'Quel jeu permet de changer les parties intimes de son personnage ?',
+        choice1: 'Battlefield 3',
+        choice2: 'Sonic Colors Ultimate',
+        choice3: 'Naruto: Ultimate Ninja Storm',
+        choice4: 'Cyberpunk 2077',
+        answer: 4,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.getElementsByClassName.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+    
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+    
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    if(!acceptingAnswers) return
+
+    acceptingAnswers = false
+    const selectedChoice = e.target
+    const selectedAnswer = selectedChoice.dataset['number']
+
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+    'incorrect'
+
+    if(classToApply === 'correct') {
+        incrementScore(SCORE_POINTS)
+    }
+    setTimeout(() => {
+        selectedChoice.parentElement.classList.add(classToApply)
+        getNewQuestion()
+    }, 1000)
+    
+})
